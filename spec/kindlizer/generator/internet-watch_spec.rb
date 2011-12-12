e8e258e1e4f65c84cacc4c805af9f623ff2f9a05
TADA Tadashi <t@tdtds.jp>
2011-12-12 18:23:23 +0900

    added 2 sample generators.
    	* nikkei-free: free articles on nikkei.com.
    	* internet-watch: all articles in the feed of InternetWatch.

---
+++ spec/kindlizer/generator/internet-watch_spec.rb
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+require File.expand_path('../../../../lib/kindlizer/generator/internet-watch', __FILE__ )
+
+describe 'internet-watch generator' do
+	context 'normal' do
+		it 'makes OPF file' do
+			Dir.mktmpdir do |dir|
+				Kindlizer::Generator::InternetWatch::new( dir ).generate do |opf|
+					opf.should eq "#{dir}/dst/internet-watch.opf"
+				end
+			end
+		end
+	end
+end
