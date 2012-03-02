c89b559ae1e5c75c75fe2a480327a38bf1f60c24
TADA Tadashi <t@tdtds.jp>
2012-03-02 18:04:28 +0900

    sparated modules.

---
+++ lib/kindlizer/backend.rb
@@ -0,0 +1,13 @@
+# -*- coding: utf-8; -*-
+#
+# configuration management
+#
+# Copyright (C) 2012 by TADA Tadashi <t@tdtds.jp>
+# Distributed under GPL.
+#
+module Kindlizer
+	module Backend
+		require 'kindlizer/backend/config'
+		require 'kindlizer/backend/task'
+	end
+end
